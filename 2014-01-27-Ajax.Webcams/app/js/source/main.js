b9947a5bb5fb3fcf461408b738cc07e804d72ec6
Adam Thede <adam@thedetech.com>
2014-01-28 20:51:19 -0600

    added web app using APIs to pull all webcams local to a particular zip code

---
+++ 2014-01-27-Ajax.Webcams/app/js/source/main.js
@@ -0,0 +1,47 @@
+(function(){
+  'use strict';
+
+  $(document).ready(initialize);
+
+  function initialize(){
+    $('#addzip').click(addZip);
+    $('#webcams').click(getWebcams);
+    $('#clear').click(clearCams);
+  }
+
+  function addZip(){
+    var zip = $('#zipcode').val();
+    $('#zipcode').val('');
+    $('#zipcode').focus();
+
+    var $option = $('<option>');
+    $option.val(zip);
+    $option.text(zip);
+
+    $('#dropdown').append($option);
+  }
+
+  function getWebcams(){
+    var zip = $('#dropdown').val();
+    var url = 'http://api.wunderground.com/api/c098c4de54fd58cb/webcams/q/CA/'+zip+'.json?callback=?';
+    $.getJSON(url, receive);
+  }
+
+  function receive(data){
+    var info = data.webcams;
+    for(var i = 0; i < info.length; i++){
+      var camData = data.webcams[i];
+      var cam = camData.WIDGETCURRENTIMAGEURL;
+      var neighborhood = camData.neighborhood;
+      var $div = $('<div>');
+      $div.addClass('cam');
+      $div.css('background-image', 'url('+cam+')');
+      $('#currentimages').prepend($div);
+    }
+  }
+
+  function clearCams(){
+    $('#currentimages').empty();
+  }
+
+})();
